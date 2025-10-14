ff928be10f986ab676788a3bcf730369ee9628f0
Ammar Arab <132087789+0ammar@users.noreply.github.com>
2025-10-14 16:54:41 +0300

    Initial

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,3 +29,4 @@ export default function RootLayout({
     </html>
   );
 }
+ 
