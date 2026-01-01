ffe2c4e56a397d8ff366ee6ad2c73fccbdc5d1d0
Ammar Arab <132087789+0ammar@users.noreply.github.com>
2026-01-01 21:45:45 +0300

    commit-36

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,18 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   sassOptions: {
-    silenceDeprecations: ['legacy-js-api'],
-    includePaths: [path.join(__dirname, 'src', 'presentation', 'styles')],
+    silenceDeprecations: ["legacy-js-api"],
+    includePaths: [path.join(__dirname, "src", "presentation", "styles")],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pub-983997e34f814b8baf6dc4b05ec7dc55.r2.dev",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
